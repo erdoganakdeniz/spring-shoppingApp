e083291d275ffbb031bc86e046827f6309ca1a5c
erdoganakdeniz <erdo44@hotmail.de>
2021-04-02 21:49:43 +0300

    yenilendi

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <Product/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Product
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  background-color: #f7fbff;
   color: #2c3e50;
   margin-top: 60px;
 }
